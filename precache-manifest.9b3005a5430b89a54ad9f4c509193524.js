a3efd9d3884a7485f4bf929d0df5cc1cd6566389
Viktor Mryga <dontworryweareinsafe@gmail.com>
2019-11-27 15:29:15 +0200

    Updates

---
+++ precache-manifest.9b3005a5430b89a54ad9f4c509193524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3106f391c3afe97d43577085572c95a6",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "78250fb209bc4a308706",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "172cb83b86668ac6935f",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.70b1c93f.chunk.css"
+  },
+  {
+    "revision": "78250fb209bc4a308706",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.3833e26a.chunk.js"
+  },
+  {
+    "revision": "172cb83b86668ac6935f",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.5be69198.chunk.js"
+  },
+  {
+    "revision": "6c3a632ae87cd989555e",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.f12b642d.js"
+  }
+]);
